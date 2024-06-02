47c1e37cbf988b631ea97f808bfa5136efb416a9
Gabrielende5 <gabrielende53@gmail.com>
2024-06-02 19:21:43 -0300

    mudando as musicas

--- songs.js
+++ songs.js
@@ -1,27 +1,63 @@
 export default [
+    // {
+    //   src: "./src/Dude - Patrick Patrikios.mp3",
+    //   name: "Dude - Patrick Patrikios",
+    // },
+    // {
+    //   src: "./src/Girl Before A Mirror - Verified Picasso.mp3",
+    //   name: "Girl Before A Mirror - Verified Picasso",
+    // },
+    // {
+    //   src: "./src/Like That - Anno Domini Beats.mp3",
+    //   name: "Like That - Anno Domini Beats",
+    // },
+    // {
+    //   src: "./src/Shake - Anno Domini Beats.mp3",
+    //   name: "Shake - Anno Domini Beats",
+    // },
+    // {
+    //   src: "./src/Turn Up Burn Up - Diamond Ortiz.mp3",
+    //   name: "Turn Up Burn Up - Diamond Ortiz",
+    // },
+    // {
+    //   src: "./src/Yo Picasso - Verified Picasso.mp3",
+    //   name: "Yo Picasso - Verified Picasso",
+    // },
     {
-      src: "./src/Dude - Patrick Patrikios.mp3",
-      name: "Dude - Patrick Patrikios",
+      src: "./src/Starlyte - When The Earth Goes Dark (feat. Brenton Mattheus) _ Ninety9Lives release (320).mp3",
+      name: "When The Earth Goes Dark",
     },
     {
-      src: "./src/Girl Before A Mirror - Verified Picasso.mp3",
-      name: "Girl Before A Mirror - Verified Picasso",
+      src:"./src/Jim Yosef - Sunrise (feat. Reece Lemonius) [Ninety9Lives] (320).mp3",
+      name: "Sunrise",
     },
     {
-      src: "./src/Like That - Anno Domini Beats.mp3",
-      name: "Like That - Anno Domini Beats",
+      src:"./src/Pegboard Nerds - Just Like That ft. Johnny Graves (Two Friends Remix) (320).mp3",
+      name: "Just Like That",
     },
     {
-      src: "./src/Shake - Anno Domini Beats.mp3",
-      name: "Shake - Anno Domini Beats",
+      src:"./src/Tristam x Karma Fields - Build The Cities (Empire Of Sound) [feat. Kerli] [Monstercat Release] (320).mp3",
+      name: "Build The Cities",
     },
     {
-      src: "./src/Turn Up Burn Up - Diamond Ortiz.mp3",
-      name: "Turn Up Burn Up - Diamond Ortiz",
+      src:"./src/Jim Yosef - Can't Wait (feat. Anna Yvette) _ House _ NCS - Copyright Free Music (320).mp3",
+      name: "Can't Wait", 
     },
     {
-      src: "./src/Yo Picasso - Verified Picasso.mp3",
-      name: "Yo Picasso - Verified Picasso",
+      src:"./src/[House] - Rich Edwards - Where I'll Be Waiting (feat. Cozi Zuehlsdorff) [Monstercat Release] (320).mp3",
+      name: "Where I'll Be Waiting",
+    },
+    {
+      src:"./src/Exit Friendzone & Murtagh - Crossfire ft. Karra (320).mp3",
+      name: "Crossfire",
+    },
+    {
+      src:"./src/Distrion & Alex Skrindo - Entropy _ House _ NCS - Copyright Free Music (320).mp3",
+      name: "Entropy",
+    },
+    {
+      src:"./src/TheFatRat - Unity (320).mp3",
+      name: "Unity",
     },
   ];
   
